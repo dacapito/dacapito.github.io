a331c28d9699b2eaf0675b72bdaa1d372d155780
dacapito <dacapito@gmail.com>
2014-03-08 16:53:33 +0100

    url

--- openMeteoForecast/scripts/install.js
+++ openMeteoForecast/scripts/install.js
@@ -1,4 +1,4 @@
-var manifest_url = 'http://dacapito.github.io/installableEbac/manifest.webapp';
+var manifest_url = 'http://dacapito.github.io/openMeteoForecast/manifest.webapp';
 
 function install(ev) {
   ev.preventDefault();
